bc0c1fa12832b154539e4b4dded044f79943ab9f
showmanpy <79121861+showmanpy@users.noreply.github.com>
2021-04-29 20:47:04 -0300

    two commit

---
+++ src/daftarvip.js
@@ -0,0 +1,20 @@
+const daftarvip = (prefix) => { 
+	return `
+	
+*PREÇO DE LISTA VIP :*
+
+-Rp. 10 > Acessar recursos ViP
+-Rp. 20 > Recursos VIP + Insira o bot no seu grupo!
+
+*SE QUER REGISTAR VIP :*
+
+*Proprietário do bate-papo BOT :*
+
+_wa.me/5565996450169 ou digite *${prefix}owner*_
+
+*NOTA*
+
+*GRUPO DO BOT:*
+_EM BREVE `
+}
+exports.daftarvip = daftarvip
---
+++ src/iklan.js
@@ -0,0 +1,18 @@
+const iklan = () => { 
+	return `           
+╔══✪〘 showmanpy 〙✪══
+║
+╠═══════════════════════════
+╠➥ *LISTA DE ALUGUEL E CRIAR BOTS:*
+╠➥ *ALUGUEL: 10 / GRUPO (MÊS)*
+╠➥ *CRIAR: 35 (PODE SER PROPRIETÁRIO)*
+╠➥ *PODE PAGAR ATRAVÉS DE:*
+╠➥ *PIX,*
+╠═══════════════════════════
+╠➥ *VANTAGENS*
+╠➥ *wa.me/5565996450169*
+║
+╚═〘 RESENHA COMPANY 〙
+`
+}
+exports.iklan = iklan
